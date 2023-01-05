380396344ef6f7a68e789d6b479af99386d3211c
Rodrigo Bersa <bersr@amazon.com>
2023-01-05 17:42:05 -0500

    Adjusing cluster-autoscaler policy

--- modules/kubernetes-addons/cluster-autoscaler/main.tf
+++ modules/kubernetes-addons/cluster-autoscaler/main.tf
@@ -71,9 +71,39 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
     resources = ["*"]
 
     actions = [
+      "ec2:DescribeInstanceTypes",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${var.addon_context.eks_cluster_id}"
+      values   = ["owned"]
+    }
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:${var.addon_context.aws_partition_id}:autoscaling:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:autoScalingGroup:*"]
+
+    actions = [
       "autoscaling:SetDesiredCapacity",
       "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "ec2:DescribeInstanceTypes",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${var.addon_context.eks_cluster_id}"
+      values   = ["owned"]
+    }
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:${var.addon_context.aws_partition_id}:eks:${var.addon_context.aws_region_name}:${var.addon_context.aws_caller_identity_account_id}:nodegroup/${var.addon_context.eks_cluster_id}/*"]
+
+    actions = [
       "eks:DescribeNodegroup",
     ]
 
